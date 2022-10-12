cc0de013a70304c09141e3e05d4d6dad5b2d989a
Marcin <mlojek@protonmail.com>
2022-10-12 14:05:34 +0200

    add CODE1.cpp and start writing code

---
+++ classical/CODE1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+
+std::array<int, 2> divComplex(std::array<int, 2> left, std::array<int, 2> right) {
+    return std::array<int, 2>{0, 0};
+}
+
+
+int main() {
+    return 0;
+}
